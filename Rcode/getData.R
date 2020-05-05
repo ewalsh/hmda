e088bd03c30ee01def660cb2db2afc9eceaed9a6
ewalsh <edmund.walsh.200@gmail.com>
2020-05-05 17:50:46 +0100

    staged with R

---
+++ Rcode/getData.R
@@ -0,0 +1,92 @@
+require(quantmod)
+require(stringr)
+### LAR data
+# get state abbreviation data
+StateAbbrData <- read.csv("~/Projects/hmda/Rcode/StateAbbrData.csv")
+# create year vector
+yrs <- seq(2007,2017,by=1)
+# create URL for api
+urlBase <- "https://api.consumerfinance.gov/data/hmda/slice/hmda_lar.csv?$where=state_abbr+%3D+'"
+urlMiddle <- "'+AND+as_of_year+%3D+"
+urlSuffix <- "&$limit=0&$offset=0"
+# start with Ohio only
+IdOhio <- 36
+url <- paste(urlBase,as.character(StateAbbrData$Abbreviation[IdOhio]),
+             urlMiddle,yrs[10],urlSuffix,sep='')
+# download
+tmpFile <- paste('./data/',as.character(StateAbbrData$Abbreviation[IdOhio]),
+                 yrs[10],'.csv',sep="")
+download.file(url,destfile=tmpFile)
+data <- read.csv(tmpFile)
+
+## CENSUS TRACTS -- NOT USEFUL
+# urlBase <- "https://api.consumerfinance.gov/data/hmda/slice/census_tracts.csv?$where=state_code+%3D+"
+# urlMiddle <- "+AND+as_of_year+%3D+"
+# urlSuffix <- "&$limit=0&$offset=0"
+# # start with Ohio only
+# IdOhio <- 36
+# url <- paste(urlBase,as.character(StateAbbrData$Abbreviation[IdOhio]),
+#              urlMiddle,yrs[10],urlSuffix,sep='')
+# # download
+# tmpFile <- paste('./data/',as.character(StateAbbrData$Abbreviation[IdOhio]),
+#                  yrs[10],'.csv',sep="")
+# download.file(url,destfile=tmpFile)
+# data <- read.csv(tmpFile)
+
+## CENSUS BUREAU 
+apiKey <- Sys.getEnv('CENSUS_API') ## replace with your own key
+urlBase <- "https://api.census.gov/data/"
+urlMiddle <- "/cbp?get=COUNTY,CSA,EMP,EMP_N,EMPSZES,ESTAB,GEO_ID,GEOTYPE,LFO,MD,MSA,NAICS2012,PAYANN,PAYANN_N,ST,YEAR&for=state:"
+tmpFile <- paste('./data/',as.character(StateAbbrData$Abbreviation[IdOhio]),
+                 yrs[10],'census','.json',sep="")
+download.file(paste(urlBase,yrs[10],urlMiddle,as.character(data$state_code[1]),sep=""),
+              destfile=tmpFile)
+censusData <- fromJSON(tmpFile)
+censusData.df <- data.frame(matrix(sapply(2:length(censusData),function(iter,censusData){
+  return((censusData[[iter]]))
+  },censusData),nrow=length(censusData)-1,byrow=TRUE),stringsAsFactors=FALSE)
+colnames(censusData.df) <- censusData[[1]]
+
+## INSTITUTIONS
+urlBase <- "https://api.consumerfinance.gov/data/hmda/slice/institutions.csv?$where=respondent_state+%3D+'"
+urlMiddle <- "'+AND+activity_year+%3D+"
+urlSuffix <- "&$limit=0&$offset=0"
+# start with Ohio only
+IdOhio <- 36
+url <- paste(urlBase,as.character(StateAbbrData$Abbreviation[IdOhio]),
+             urlMiddle,yrs[10],urlSuffix,sep='')
+# download
+tmpFile <- paste('./data/',as.character(StateAbbrData$Abbreviation[IdOhio]),
+                 yrs[10],'insts.csv',sep="")
+download.file(url,destfile=tmpFile)
+data <- read.csv(tmpFile)
+
+## ECONOMIC DATA
+# delinquency rate on single family residential mortgages
+DRSFRMACBS = data.frame(getSymbols("DRSFRMACBS",src="FRED",auto.assign=FALSE))
+diffLogData <- diff(log(DRSFRMACBS[,1]))
+normDelinq <- data.frame(DelinquencyRate=(diffLogData-mean(diffLogData))/sd(diffLogData),
+                         row.names=row.names(DRSFRMACBS)[-1])
+# find year and month/quarter
+normDelinq <- cbind(normDelinq,data.frame(year=as.numeric(str_sub(row.names(normDelinq),1,4)),
+                                          month=as.numeric(str_sub(row.names(normDelinq),6,7)),
+                                          type=rep('delinquency_rate',nrow(normDelinq)))
+)
+# mortgage debt outstanding
+MDOAH = data.frame(getSymbols("MDOAH",src="FRED",auto.assign=FALSE))
+diffLogData <- diff(log(MDOAH[,1]))
+mortDebtOut <- data.frame(MortDebtOut=(diffLogData-mean(diffLogData,na.rm=TRUE))/sd(diffLogData,na.rm=TRUE),
+                         row.names=row.names(MDOAH)[-1])
+# find year and month/quarter
+mortDebtOut <- cbind(mortDebtOut,data.frame(year=as.numeric(str_sub(row.names(mortDebtOut),1,4)),
+                                          month=as.numeric(str_sub(row.names(mortDebtOut),6,7)),
+                                          type=rep('mort_debt_out',nrow(mortDebtOut)))
+)
+# net percentage of domestic banks tightening standards for auto loans
+STDSAUTO = data.frame(getSymbols("STDSAUTO",src="FRED",auto.assign=FALSE))
+
+# find year and month/quarter
+autoStandards <- cbind(STDSAUTO,data.frame(year=as.numeric(str_sub(row.names(STDSAUTO),1,4)),
+                                          month=as.numeric(str_sub(row.names(STDSAUTO),6,7)),
+                                          type=rep('auto_standards',nrow(STDSAUTO)))
+)
